25b32df46496e71223fc57ec97989f37bcbe2104
eritainn7 <eritainn7@gmail.com>
2026-02-27 11:16:44 +0300

    feat: complete 4 task in common task list

---
+++ lab1/general/4.R
@@ -0,0 +1,10 @@
+vec3 <- seq(from=3, to=27, by=3)
+print(vec3[c(2, 5, 7)])
+print(vec3[length(vec3) - 1])
+print(vec3[-length(vec3) + 1])
+print(vec3[-6])
+print(vec3[100])
+print(vec3[-c(1, length(vec3))])
+print(vec3[(4 < vec3) & (10 > vec3)])
+print(vec3[vec3 < 4 | vec3 > 10])
+  
