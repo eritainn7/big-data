3c917c1c2b90e1d6ce644ed30b44e1a8a67b347c
eritainn7 <eritainn7@gmail.com>
2026-02-27 12:05:37 +0300

    feat: load data from csv

--- lab2/main.R
+++ lab2/main.R
@@ -1,12 +1,3 @@
 data <- read.csv("/home/alex/kubsu/big-data/lab2/data.csv")
 berrys <- names(data)[-1]
 data <- data[,-1]
-#1
-print(colMeans(data, na.rm=TRUE))
-print(sapply(data, max, na.rm=TRUE))
-print(sapply(data, min, na.rm=TRUE))
-
-
-#3
-ratings <- colMeans(data, na.rm=TRUE)
-print(sort(ratings, decreasing=TRUE))
