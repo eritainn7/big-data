a00555dec36b5b3a0220f5533ba0dc81075e141f
eritainn7 <sanyapridava@mail.ru>
2026-03-27 00:25:31 +0300

    feat: create package with method for create plot

---
+++ lab4/R/view/plots.R
@@ -0,0 +1,16 @@
+source("~/kubsu/big-data/lab4/R/data/dataframes.R")
+
+countries <- c("Ukraine", "Georgia", "Greece", "Israel", "Netherlands", "Qatar", "Jordan")
+years <- 2014:2026
+source_data <- "https://www.numbeo.com/quality-of-life/rankings_by_country.jsp?title=" #После title добавить соответствующий год
+
+dataframes_with_indexes <- getDataForCountries(countries, years, source_data) # Получаем данные для построения графиков
+length(dataframes_with_indexes)
+
+
+#Функция для построения графиков изменения индексов стран
+generatePlots <- function() {
+  
+}
+
+
