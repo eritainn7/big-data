4f3ad3bcdb72b51d9ad343c46e347c6ee4609784
eritainn7 <sanyapridava@mail.ru>
2026-03-27 02:36:14 +0300

    refactor:

--- lab4/R/data/dataframes.R
+++ lab4/R/data/dataframes.R
@@ -6,16 +6,23 @@ getDataForCountries <- function(countries, years, source_data) {
   for (year in years) {
     html_page <-read_html(paste(source_data, year, sep = ''))
     nodes <- html_nodes(html_page, 'table')
-    
+
     table_data_year <- as.data.frame(html_table(nodes[2]))
     table_data_year <- table_data_year[table_data_year$Country %in% countries,]
-    
-    #Добавим к наименованиям год, чтоб не было перекрытий
-    
-    
+
     result <- append(result, table_data_year)
   }
- 
+  
+  new_names <- c()
+  for (year in years) {
+    for (name in unique(names(result))) {
+      new_name <- paste(name, year)
+      new_names <- append(new_names, new_name)
+    }
+  }
+  names(result) <- new_names
+  
   return(result)
 }
+
 
