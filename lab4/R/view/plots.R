03d1993a828691be704452053c86817bac805954
eritainn7 <sanyapridava@mail.ru>
2026-03-27 02:37:19 +0300

    refactor:

--- lab4/R/view/plots.R
+++ lab4/R/view/plots.R
@@ -4,10 +4,9 @@ countries <- c("Ukraine", "Georgia", "Greece", "Israel", "Netherlands", "Qatar",
 years <- 2014:2026
 source_data <- "https://www.numbeo.com/quality-of-life/rankings_by_country.jsp?title=" #После title добавить соответствующий год
 
+#Данные для построения графиков
 dataframes_with_indexes <- getDataForCountries(countries, years, source_data) # Получаем данные для построения графиков
-length(dataframes_with_indexes)
-
-
+dataframes_with_indexes
 #Функция для построения графиков изменения индексов стран
 generatePlots <- function() {
   
