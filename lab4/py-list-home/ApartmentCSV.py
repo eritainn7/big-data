7ce15971d015bac44ccf8ca91318bf17b2f98906
eritainn7 <sanyapridava@mail.ru>
2026-04-10 13:25:54 +0300

    feat: begin work about Saving data in csv

---
+++ lab4/py-list-home/ApartmentCSV.py
@@ -0,0 +1,9 @@
+from Apartment import Apartment
+import csv
+
+class ApartmentCSV:
+    def __init__(self, aprt: Apartment):
+        self.__aprt = aprt
+        
+    def saveToCSV():
+        pass
