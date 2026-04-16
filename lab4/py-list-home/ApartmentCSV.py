4b95c7cd9d41b03ebd6cf4051b2951fe4e21bf19
eritainn7 <sanyapridava@mail.ru>
2026-04-16 23:54:18 +0300

    feat: add saving data of apartments in csv

--- lab4/py-list-home/ApartmentCSV.py
+++ lab4/py-list-home/ApartmentCSV.py
@@ -2,8 +2,14 @@ from Apartment import Apartment
 import csv
 
 class ApartmentCSV:
-    def __init__(self, aprt: Apartment):
-        self.__aprt = aprt
+    def __init__(self, DATA_APARTS):
+        self.__DATA_APARTS = DATA_APARTS
         
-    def saveToCSV():
-        pass
+    def saveToCSV(self, filename: str):
+        with open(f'{filename}.csv', 'w', newline='', encoding='utf-8') as file:
+            fieldnames = self.__DATA_APARTS[0].keys()
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            
+            writer.writeheader()
+            
+            writer.writerows(self.__DATA_APARTS)
